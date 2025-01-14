6f32c07d7a88ca2bef680a1a35e057f96b470ab7
Gonzalo Muñoz <gonzodl38@santafe.com>
2025-01-14 10:04:24 -0500

    chore: files structure and initial files

---
+++ js/helloworld.js
@@ -0,0 +1,10 @@
+console.log("dfasddsa");
+const firebaseConfig = {
+    apiKey: "<YOUR_GoogleAPIKey_HERE>",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  };
