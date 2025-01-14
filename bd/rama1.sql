365e1c31f362b4e50f2ea025efef7538c013d928
Gonzalo Muñoz <gonzodl38@santafe.com>
2025-01-14 11:23:47 -0500

    feat: add table tabla3

--- bd/rama1.sql
+++ bd/rama1.sql
@@ -1,3 +1,10 @@
-create table rama1(
+create table tabla1(
     id int,
-    name varchar(20)
+    name varchar(20);
+
+create table tabla2(
+    id int,
+    name varchar(20);
+create table tabla3(
+    id int,
+    name varchar(20);
