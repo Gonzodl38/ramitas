33a56afb240fa8bb746b436092394450174d14c8
Gonzalo Muñoz <gonzodl38@santafe.com>
2025-01-14 11:26:51 -0500

    feat: update rama2.sql

--- bd/rama2.sql
+++ bd/rama2.sql
@@ -1,7 +1,14 @@
-create table rama2(
+create table tabla4(
     id int not null auto_increment,
     nombre varchar(50) not null,
     apellido varchar(50) not null,
     edad int not null,
     primary key(id)
+)
+create table payroll(
+    id int not null auto_increment,
+    id_empleado int not null,
+    salario int not null,
+    primary key(id),
+    foreign key(id_empleado) references tabla4(id)   
 )
