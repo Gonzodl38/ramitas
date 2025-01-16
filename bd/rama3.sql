76bc2314967282b027f7ff60aeb3a39e6224fa49
Gonzalo Muñoz <gonzodl38@yahoo.com>
2025-01-16 07:10:17 -0500

    feat: add file rama3

---
+++ bd/rama3.sql
@@ -0,0 +1,8 @@
+crete table employees(
+    id int not null auto_increment,
+    name varchar(255) not null,
+    address varchar(255) not null,
+    salary int not null,
+    age int not null,
+    image varchar(255),
+    primary key(id));
